a967c7af77d7baf3046f8c7c34dbed3d5f525c21
wangxing-hub <76116792+wangxing-hub@users.noreply.github.com>
2023-11-03 23:24:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <br><el-button type="primary" @click="hey">hey</el-button>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   methods: {
-    hey() {
-      alert('hey')
-    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
