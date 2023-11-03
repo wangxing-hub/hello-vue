bea682070c8171c42cc08d01bf28404a280f0ae6
wangxing-hub <76116792+wangxing-hub@users.noreply.github.com>
2023-11-03 23:23:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import router from '@/router/index.ts'
 
 // createApp(App).use(ElementPlus).mount('#app')
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
