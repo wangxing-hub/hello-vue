9b6272e4fb47c61b92dbbbe5ecbf87556de30425
wangxing <2842175271@qq.com>
2023-11-04 12:58:44 +0800

    解决报错

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,24 +3,27 @@ import { createRouter,createWebHashHistory } from 'vue-router'
 const homePage = ()=> import('@/views/homePage.vue')
 const todoList = ()=> import('../views/todoList.vue')
 
-// 路由表
-const routes = [
-    {
-        path:'/homePage',
-        component:homePage
-    },
-    {
-        path:'/todoList',
-        name: 'todoList',
-        // 懒加载
-        component: () => import("../views/todoList.vue")
-    }
-]
-
 // 实例化路由对象，使用hash模式
 const router = createRouter({
     history:createWebHashHistory(),
-    routes
+    // 路由表
+    routes: [
+        {
+            path: '/',
+            redirect: '/homePage'
+        },
+        {
+            path:'/homePage',
+            component:homePage
+        },
+        {
+            path:'/todoList',
+            name: 'todoList',
+            // 懒加载
+            component: () => import("../views/todoList.vue")
+        }
+    ]
+    
 })
 
 export default router
